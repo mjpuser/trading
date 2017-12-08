5782a1eeb34306cdd2775eb3f59b930d1cb15ead
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2017-12-08 17:41:10 -0500

    tree is searching, returning paths

--- test/unit/lib/test_tree.py
+++ test/unit/lib/test_tree.py
@@ -12,6 +12,11 @@ class TreeTestCase(unittest.TestCase):
         self.tree.add_child(node)
         self.assertTrue(node in self.tree.children)
 
+    def test_child_node_has_parent(self):
+        node = trading.lib.tree.Node()
+        self.tree.add_child(node)
+        self.assertEquals(node.parent, self.tree)
+
     def test_node_with_no_children_is_empty_list(self):
         self.assertEquals(self.tree.children, [])
 
@@ -19,6 +24,18 @@ class TreeTestCase(unittest.TestCase):
         value = 'test'
         node = trading.lib.tree.Node(value)
         self.assertEquals(node.value, value)
+
+    def test_node_is_leaf(self):
+        value = 'test'
+        node = trading.lib.tree.Node(value)
+        self.assertTrue(node.is_leaf())
+
+    def test_node_is_not_leaf(self):
+        value = 'test'
+        node = trading.lib.tree.Node(value)
+        self.assertTrue(node.is_leaf())
+        node.add_child(node)
+        self.assertFalse(node.is_leaf())
 
     def test_only_accept_child_node_type(self):
         value = None
@@ -29,3 +46,77 @@ class TreeTestCase(unittest.TestCase):
             'Child node must be type trading.lib.tree.Node not {}'.format(value.__class__.__name__)
         )
         self.assertTrue(isinstance(context.exception, ValueError))
+
+    def test_accept_tree_dict_structure(self):
+        structure = {
+            'value': 1,
+            'children': [
+                { 'value': 2 },
+                { 'value': 3 },
+                {
+                    'value': 4,
+                    'children': [
+                        { 'value': 5 }
+                    ]
+                }
+            ]
+        }
+        tree = trading.lib.tree.Tree(structure)
+        self.assertTrue(tree.value, 1)
+        self.assertTrue(tree.children[0].value, 2)
+        self.assertTrue(tree.children[1].value, 3)
+        self.assertTrue(tree.children[2].value, 4)
+        self.assertTrue(tree.children[2].children[0].value, 5)
+
+
+class TreeTraverseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.searcher = trading.lib.tree.Searcher()
+        self.tree = trading.lib.tree.Tree()
+
+    def build_tree(self):
+        #               1
+        #          /        \
+        #      2               6
+        #    /   \         /   \    \
+        #  3      4      7       9    10
+        #        /        \
+        #       5          8
+        structure = {
+            'value': 1,
+            'children': [
+                { 'value': 2, 'children': [
+                    { 'value': 3 },
+                    { 'value': 4, 'children': [
+                        { 'value': 5 }
+                    ] }
+                ] },
+                { 'value': 6, 'children': [
+                    { 'value': 7, 'children': [
+                        { 'value': 8 }
+                    ] },
+                    { 'value': 9 },
+                    { 'value': 10 }
+                ] }
+            ]
+        }
+        tree = trading.lib.tree.Tree(structure)
+        return tree
+
+    def test_depth_first_search(self):
+        tree = self.build_tree()
+        results = [ x.value for x in self.searcher.search(tree) ]
+        self.assertEquals(results, [ x for x in range(1, 11) ])
+
+    def test_option_to_emit_paths(self):
+        tree = self.build_tree()
+        paths = [
+            (1, 2, 3,),
+            (1, 2, 4, 5,),
+            (1, 6, 7, 8,),
+            (1, 6, 9,),
+            (1, 6, 10,),
+        ]
+        results = [ tuple(map(lambda x: x.value, x)) for x in self.searcher.paths(tree) ]
+        self.assertEquals(results, paths)
--- trading/lib/tree.py
+++ trading/lib/tree.py
@@ -1,20 +1,51 @@
 
-class Node:
+class Node(object):
 
     def __init__(self, value=None, children=None):
         self.children = children if type(children) == list else []
         self.value = value
+        self.parent = None
 
     def add_child(self, node):
         if isinstance(node, Node):
             self.children.append(node)
+            node.parent = self
         else:
+            node_type = '{}.{}'.format(__name__, Node.__name__)
             curr_type = getattr(node, '__module__', '')
             class_name = node.__class__.__name__
             if curr_type is not '':
                 curr_type += '.'
             curr_type += class_name
-            raise ValueError('Child node must be type {} not {}'.format(Node, curr_type))
+            raise ValueError('Child node must be type {} not {}'.format(node_type, curr_type))
+
+    def is_leaf(self):
+        return len(self.children) == 0
+
 
 class Tree(Node):
-    pass
+
+    def __init__(self, structure=None):
+        super(Tree, self).__init__()
+        if structure is not None:
+            self.value = structure.get('value')
+            for child in structure.get('children', []):
+                self.add_child(Tree(child))
+
+
+class Searcher:
+
+    def search(self, node):
+        yield node
+        for child in node.children:
+            for child in self.search(child):
+                yield child
+
+    def paths(self, node):
+        for child in self.search(node):
+            if child.is_leaf():
+                path = []
+                while child is not None:
+                    path.insert(0, child)
+                    child = child.parent
+                yield tuple(path)
