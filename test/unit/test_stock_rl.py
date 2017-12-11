dcd0ae07f9d784b1f76ef8fef5103a5c9bdb03ff
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2017-12-11 17:53:14 -0500

    testing horizon staggered

--- test/unit/test_stock_rl.py
+++ test/unit/test_stock_rl.py
@@ -18,3 +18,15 @@ class StateGeneratorTestCase(unittest.TestCase):
             (1, 0, 0, BUY,), (1, 1, 1, NOOP,), (1, 1, 3, SELL,),
         ]
         self.assertEqual(expected, paths)
+
+    def test_staggered_paths(self):
+        data = [(1,), (1,), (2,),]
+        paths = [
+            path
+            for path in trading.stock_rl.state_generator(data, horizon=2)
+        ]
+        expected = [
+            (1, 0, 0, BUY,), (1, 1, 1, SELL,),
+            (1, 0, 0, BUY,), (2, 1, 2, SELL,),
+        ]
+        self.assertEqual(expected, paths)
