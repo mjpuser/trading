ff001bb62ea410dba0f2e4e70a49639188123f27
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2017-12-30 17:22:49 -0500

    redoing stock_rl

--- test/unit/test_stock_rl.py
+++ test/unit/test_stock_rl.py
@@ -2,85 +2,29 @@ import unittest
 import trading.stock_rl
 import test.unit.data
 
-BUY = trading.stock_rl.ACTION['buy']
-SELL = trading.stock_rl.ACTION['sell']
-WAIT = trading.stock_rl.ACTION['wait']
-PROMISE = trading.stock_rl.ACTION['promise']
-revmap = trading.stock_rl.revmap
+BOLLINGER = trading.stock_rl.BOLLINGER
+ACTION = trading.stock_rl.ACTION
 
 class StateGeneratorTestCase(unittest.TestCase):
+    def test_discretizer(self):
+        # takes change, bollinger, owns, buystate, return, action
+        # returns to bollinger, owns, buystate, action
+        state = tuple([
+            0.01,
+            'inside',
+            True,
+            'above',
+            0.10,
+            'buy',
+        ])
 
-    def test_paths(self):
-        # b s - - - -
-        # b s b s - -
-        # b s b s b s
-        # b s b - s -
-        # b s b - - s
-        # b s - b s -
-        # b s - b - s
-        # b s - - b s
-        # b - s - - -
-        # b - s b s -
-        # b - s b - s
-        # b - s - b s
-        # b - - s - -
-        # b - - s b s
-        # b - - - s -
-        # - b s - - -
-        # - b s b s -
-        # - b s b - s
-        # - b s - b s
-        # - b - s - -
-        # - b - s b s
-        # - b - - s -
-        # - - b s - -
-        # - - b s b s
-        # - - b - s -
-        # - - b - - s
-        # - - - b s -
-        # - - - b - s
-        # - - - - b s
-        data = [(0, 0,), (0, 1,), (0, 2), (0, 0,)]
-        expected = [
-            (0, 0, 0, 0, BUY,), (0, 1, 1, 0, SELL),
-            (0, 0, 0, 0, BUY,), (0, 1, 1, 0, SELL), (0, 2, 0, 0, BUY), (0, 0, 1, 0, SELL),
-            (0, 0, 0, 0, BUY,), (0, 1, 1, 0, PROMISE), (0, 2, 2, 0, SELL),
-            (0, 0, 0, 0, BUY,), (0, 1, 1, 0, PROMISE), (0, 2, 2, 0, PROMISE), (0, 0, 3, 0, SELL),
-            (0, 0, 0, 0, WAIT,), (0, 1, 0, 0, WAIT), (0, 2, 0, 0, BUY), (0, 0, 1, 0, SELL),
-            (0, 0, 0, 0, WAIT), (0, 1, 0, 0, BUY), (0, 2, 1, 0, PROMISE), (0, 0, 2, 0, SELL),
-            (0, 0, 0, 0, WAIT), (0, 1, 0, 0, BUY), (0, 2, 1, 0, SELL),
-        ]
-        paths = [
-            path
-            for path in trading.stock_rl.state_generator(data, horizon=4)
-        ]
-        print('paths', paths)
-        self.assertEqual(expected, paths)
+        dstate = trading.stock_rl.discretize(state)
 
-    def test_learning(self):
-        data = test.unit.data.data
+        expected = tuple([
+            BOLLINGER['inside'],
+            True,
+            BOLLINGER['above'],
+            ACTION['buy'],
+        ])
 
-        learner = trading.stock_rl.Learner()
-        learner.learn(
-            lambda: trading.stock_rl.state_generator(data, horizon=6),
-            iterations=1,
-            alpha=0.3,
-            gamma=0.7
-        )
-
-        states = [
-            (0, 0, 0, 0, None,),
-            (-0.5, 1, 0, 0, None,),
-            (1, 0, 1, 0, None,),
-            (1, 0, 1, 1, None,),
-        ]
-        expecteds = [
-            trading.stock_rl.DACTION['buy'],
-            trading.stock_rl.DACTION['wait'],
-            trading.stock_rl.DACTION['sell'],
-            trading.stock_rl.DACTION['sell'],
-        ]
-        for index, state in enumerate(states):
-            *_, action = learner.argmax(learner.discretize(state))
-            print(state)
-            self.assertEqual(revmap[str(action)], revmap[str(expecteds[index])])
+        self.assertEqual(dstate, expected)
--- trading/stock_rl.py
+++ trading/stock_rl.py
@@ -14,92 +14,29 @@ COL = {
 
 # index for action
 ACTION = {
-    'promise': 'promise',
-    'buy': 'buy',
-    'wait': 'wait',
-    'sell': 'sell',
-}
-
-DAYS_OWNED_SIZE = 2
-
-# index for action
-DACTION = {
     'promise': 0,
     'buy': 1,
     'sell': 2,
     'wait': 3,
 }
 
-revmap = {
-    '0': 'promise',
-    '1': 'buy',
-    '2': 'sell',
-    '3': 'wait'
+BOLLINGER = {
+    'inside': 0, # inside
+    'below': 1, # below the band
+    'dipped': 2, # went from below to inside
+    'above': 3, # above the band
+    'returned': 4, # went from above to inside
 }
 
-
-discrete_changes = [
-    -0.013705409864274043,
-    -0.007994551909556669,
-    -0.004127470678394696,
-    -0.001805747215627207,
-    0.00038355921955979255,
-    0.0028797863193601447,
-    0.005756997975481681,
-    0.009824218198390078,
-    0.015323150458234291,
-]
-
-def discretize_change(change):
-    for i, bound in enumerate(discrete_changes):
-        if change < bound:
-            return i
-    return len(discrete_changes)
-
-
-def discretize_return(change):
-    return discretize_change(change)
-    if -0.005 < change < 0:
-        x = 11
-    elif 0 <= change < 0.005:
-        x = 0
-
-    elif 0.005 <= change < 0.015:
-        x = 6
-    elif 0.015 <= change < 0.025:
-        x = 7
-    elif 0.025 <= change < 0.035:
-        x = 8
-    elif 0.035 <= change < 0.045:
-        x = 9
-    elif 0.045 <= change:
-        x = 10
-
-    elif -0.015 < change <= -0.005:
-        x = 1
-    elif -0.025 < change <= -0.015:
-        x = 2
-    elif -0.035 < change <= -0.025:
-        x = 3
-    elif -0.045 < change <= -0.035:
-        x = 4
-    elif change <= -0.045:
-        x = 5
-    return x
+revmap = list(ACTION.keys())
 
 def discretize_action(action):
     # return None in case invalid action
-    return action
+    return ACTION[action]
 
-def discretize_owns(days):
-    return min(days, DAYS_OWNED_SIZE - 1)
-# states is % change, bollinger, owns, return, action
-# b - - - s b - - - s
-# - b - - - s b - - - s
-# - - b - - - s b - - - s
-# - - - b - - - s b - - - s
-# - - - - b - - - s b - - - s
-# b - - - s - - - b
+def discretize_owns(owns):
+    return owns
+
 def state_generator(states, learner, randomness=1):
     ret = 0
     days_owned = 0
@@ -114,12 +51,6 @@ def state_generator(states, learner, randomness=1):
         else:
             *_, action = learner.argmax(learner.discretize((change, bollinger, days_owned, ret, None)))
         yield change, bollinger, days_owned, ret, action
-        # print('----------------')
-        # print('change', change)
-        # print('bollinger', bollinger)
-        # print('owned', days_owned)
-        # print('return', ret)
-        # print('action', revmap[str(action)], allowed_actions)
         if action == DACTION['buy']:
             days_owned = 1
             ret = 0
@@ -142,12 +73,12 @@ def actions_filter(state):
         return (DACTION['wait'], DACTION['buy'],)
 
 def discretize(state):
-    change, bollinger, owns, ret, action = state
+    # change, bollinger, owns, buystate, return, action
+    _, bollinger, owns, buystate, _, action = state
     return (
-        discretize_change(change),
-        bollinger,
+        BOLLINGER[bollinger],
         discretize_owns(owns),
-        discretize_return(ret),
+        BOLLINGER[buystate],
         discretize_action(action),
     )
 
